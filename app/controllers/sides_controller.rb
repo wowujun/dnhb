966f88ae25386592ef0949b9940d357342d1456f
Wu Jun <499675898@qq.com>
2015-12-28 11:15:04 +0800

    ceshi1

--- app/controllers/sides_controller.rb
+++ app/controllers/sides_controller.rb
@@ -6,14 +6,14 @@ class SidesController < ApplicationController
     @advertisements = Advertisement.all
     @sidecontents = Sidecontent.all.paginate(page: params[:page],per_page:4)
     @sidecontent =  Sidecontent.find_by_sql("SELECT * FROM sidecontents LIMIT 6")
-    @sideproducts = Sideproduct.all
     @sideabouts = Sideabout.all
-    @sideproduct = Sideproduct.find_by_sql("SELECT * FROM sideproducts LIMIT 6")
+
   end
 
   def show
     @advertisements = Advertisement.all
     @sidecfgs = Sidecfg.all
+
      if params[:cla]=='product'
        @sidecla = Sidecla.find(params[:id])
        @sideproducts =@sidecla.sideproducts
