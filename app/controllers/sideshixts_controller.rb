d6e41dc0c3cb7466409143f1598a44d165d8b1df
Wu Jun <499675898@qq.com>
2016-01-13 10:41:06 +0800

    asdasdhjaksdhj

--- app/controllers/sideshixts_controller.rb
+++ app/controllers/sideshixts_controller.rb
@@ -6,7 +6,9 @@ class SideshixtsController < ApplicationController
   # GET /sideshixts
   # GET /sideshixts.json
   def index
-    @sideshixts = Sideshixt.all.paginate(page: params[:page],per_page:15).order("date desc")
+    # sideshixt = Sideshixt.find_by_sql("select *from (select * from sideshixts order by date desc) c order by status desc")
+    sideshixt = Sideshixt.order("date desc")
+@sideshixts = sideshixt.paginate(page: params[:page],per_page:20).order("status desc")
   end
 
   # GET /sideshixts/1
