8096a55d2dba91108c5aa4d48b3fde77b5e2c682
Wu Jun <499675898@qq.com>
2016-01-14 15:27:08 +0800

    asdadsas

--- app/controllers/sides_controller.rb
+++ app/controllers/sides_controller.rb
@@ -9,9 +9,9 @@ class SidesController < ApplicationController
     @sidecontent =  Sidecontent.find_by_sql("SELECT * FROM sidecontents LIMIT 6")
     @sideabouts = Sideabout.all
 
-    sideshixt = Sideshixt.find_by_sql("select *from (select * from sideshixts order by date desc) c order by status desc")
-    #sideshixt = Sideshixt.order("date desc")
-    @sideshixts = Sideshixt.paginate(page: params[:sideshixt],per_page:20).order("id desc")
+
+
+    @sideshixts = Sideshixt.paginate(page: params[:page],per_page:20).order("status desc,date desc")
 
     @classcas = Classca.all
   end
--- app/controllers/sideshixts_controller.rb
+++ app/controllers/sideshixts_controller.rb
@@ -6,9 +6,36 @@ class SideshixtsController < ApplicationController
   # GET /sideshixts
   # GET /sideshixts.json
   def index
-     sideshixt = Sideshixt.find_by_sql("select *from (select * from sideshixts order by date desc) c order by status desc")
-    #sideshixt = Sideshixt.order("date desc")
-@sideshixts = Sideshixt.paginate(page: params[:sideshixt],per_page:20).order("id desc")
+    #@sideshixts = Sideshixt.all.find(,:conditions => ["select *from(select * from sideshixts order by date desc) c order by status desc"]).paginate(page: params[:page],per_page:2)
+
+    #@sideshixt = Sideshixt.find_by_sql("select *from(select * from sideshixts order by date desc) b order by status desc")
+    @sideshixts = Sideshixt.paginate(page:params[:page],per_page:15).order("status desc,date desc")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      # @sideshixt = Sideshixt.order(date: :asc)
+    # @sideshixts =  @sideshixt.paginate(page: params[:page],per_page:20).order(status: :asc)
+   #@sideshixts = @sideshixts.paginate(page: params[:page],per_page:2)
   end
 
   # GET /sideshixts/1
